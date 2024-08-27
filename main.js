6da92f78c98c75a3d3adb79ca21bb595fa7ca12a
NgTZ <warinpalm@gmail.com>
2024-08-27 11:35:14 +0700

    add card to 6

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
-(function(){
-    var myBlog =[
-        {
-            title : "Sofa1",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa1.avif",
-            link : "#",
+(function() {
+    var myBlog = [{
+            title: "Sofa1",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa1.avif",
+            link: "#",
         },
         {
-            title : "Sofa2",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa2.avif",
-            link : "#",
+            title: "Sofa2",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa2.avif",
+            link: "#",
         },
         {
-            title : "Sofa3",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa1.avif",
-            link : "#",
+            title: "Sofa3",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa1.avif",
+            link: "#",
         },
         {
-            title : "Sofa4",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa1.avif",
-            link : "#",
+            title: "Sofa4",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa1.avif",
+            link: "#",
         },
         {
-            title : "Sofa5",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa1.avif",
-            link : "#",
+            title: "Sofa5",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa1.avif",
+            link: "#",
         },
         {
-            title : "Sofa6",
-            text : "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
-            image : "image/sofa1.avif",
-            link : "#",
+            title: "Sofa6",
+            text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+            image: "image/sofa1.avif",
+            link: "#",
         },
 
     ];
 
-var blog = document.getElementById("myListBlog");
-var innerHTML = "";
-for(let i = 0 ; i < myBlog.length; i++){
-    let data = myBlog[i];
-    innerHTML += `
+    var blog = document.getElementById("myListBlog");
+    var innerHTML = "";
+    for (let i = 0; i < myBlog.length; i++) {
+        let data = myBlog[i];
+        innerHTML += `
 
         <div class="card" style="width: 18rem;">
           <img src="${data.image}" class="card-img-top" alt="...">
@@ -56,7 +55,6 @@ for(let i = 0 ; i < myBlog.length; i++){
           </div>
         </div>  
     `;
-}   
-blog.innerHTML = innerHTML;
+    }
+    blog.innerHTML = innerHTML;
 })();
-
